b3af28438d379ab4b44f673b78dcc0f64361ff48
cp-14syf <cp-14syf@PC-BD18-03.intra.ceff.ch>
2017-05-10 10:29:17 +0200

    Ajout methode addCarte dans le controleur

--- src/ch/ceff/ict3/sdajcemanager/controleurs/Controleur.java
+++ src/ch/ceff/ict3/sdajcemanager/controleurs/Controleur.java
@@ -5,10 +5,25 @@
  */
 package ch.ceff.ict3.sdajcemanager.controleurs;
 
+import ch.ceff.ict3.sdajcemanager.event.AddCarteEvent;
+import ch.ceff.ict3.sdajcemanager.modele.Carte;
+import ch.ceff.ict3.sdajcemanager.modele.Database;
+
 /**
  *
  * @author cp-14luf
  */
 public class Controleur {
-    
+
+    private Database db;
+
+    public Controleur() {
+        db = db.newDatabase();
+    }
+
+
+    public void AddCarte(AddCarteEvent event) {
+        Carte carte = new Carte(-1,event.getNom(),event.getType(), event.getSphere(), event.getNombre(), event.getConteneur());
+        db.addCarte(carte);
+    }
 }
