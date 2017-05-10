d560f0e546e0127ff3c52d65c9a94e18b7c52c60
cp-13jru <cp-13jru@PC-BD18-01.intra.ceff.ch>
2017-05-10 11:33:47 +0200

    ajout du fichier pour l'interface des différents options pour la page carte ajout des différents boutons et textField définition de la taille du panneau placement des éléments dans le gridBag

---
+++ src/ch/ceff/ict3/sdajcemanager/composants/FormPanelCarte.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.ceff.ict3.sdajcemanager.composants;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+/**
+ *
+ * @author cp-13jru
+ */
+public class FormPanelCarte extends JPanel {
+
+    private JButton buttonSearch;
+    private JButton buttonAddCart;
+    private JComboBox comboType;
+    private JComboBox comboAttribut;
+    private JComboBox comboProperty;
+    private JTextField searchField;
+
+    public FormPanelCarte() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        //liste des types
+
+        comboType = new JComboBox();
+        DefaultComboBoxModel modelType = new DefaultComboBoxModel();
+        modelType.addElement("Type");
+        modelType.addElement("Heros");
+        modelType.addElement("Evenement");
+        modelType.addElement("Attachement");
+        modelType.addElement("Allié");
+        comboType.setModel(modelType);
+        comboType.setSelectedIndex(0);
+        comboType.setEditable(true);
+        //liste des sphère
+
+        comboAttribut = new JComboBox();
+        DefaultComboBoxModel modelSphere = new DefaultComboBoxModel();
+        modelSphere.addElement("Sphere");
+        modelSphere.addElement("Tactique");
+        modelSphere.addElement("Connaissance");
+        modelSphere.addElement("Energie");
+        modelSphere.addElement("Commandement");
+        modelSphere.addElement("Neutre");
+        comboAttribut.setModel(modelSphere);
+        comboAttribut.setSelectedIndex(0);
+        comboAttribut.setEditable(true);
+        
+        //Liste des Conteneur
+        comboProperty = new JComboBox();
+        DefaultComboBoxModel modelConteneur = new DefaultComboBoxModel();
+        modelConteneur.addElement("Conteneur");
+        modelConteneur.addElement("BB");
+        modelConteneur.addElement("C1");
+        comboProperty.setModel(modelConteneur);
+        comboProperty.setSelectedIndex(0);
+        comboProperty.setEditable(true);
+
+        Dimension dimensions = new Dimension(250,150);
+        setPreferredSize(dimensions);
+        Border bordure = BorderFactory.createTitledBorder("Option");
+        Border marge = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+        setBorder(BorderFactory.createCompoundBorder(marge, bordure));
+
+        searchField = new JTextField(9);
+        buttonSearch = new JButton("Rechercher");
+        buttonAddCart = new JButton("Ajouter carte");
+
+        //placement des composants
+        layoutComponents();
+
+        //action sur le bouton rechercher
+        buttonSearch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        //action sur le bouton ajouter carte
+        buttonAddCart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+    }
+
+    private void layoutComponents() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+        
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        gc.weightx = 1;
+        gc.weighty = 0.1;
+
+        //ligne 1
+        gc.gridx = 0;
+        gc.gridy = 0;
+
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        gc.anchor = GridBagConstraints.FIRST_LINE_END;
+        add(new JLabel("Recherche :"), gc);
+
+        
+        gc.gridx = 1;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gc.insets =  new Insets(0,0,0,3);
+        add(searchField, gc);
+        
+        gc.gridx = 2;
+        gc.anchor = GridBagConstraints.FIRST_LINE_END;
+        add(buttonSearch, gc);
+        
+        gc.gridx=3;
+        gc.anchor = GridBagConstraints.FIRST_LINE_END;
+        add(buttonAddCart,gc);
+        
+        //ligne 2
+        gc.gridx=0;
+        gc.gridy=1;
+        
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        add(comboType,gc);
+        
+        gc.gridx = 1;
+        gc.anchor = GridBagConstraints.PAGE_START;
+        gc.insets =  new Insets(0,0,0,3);
+        add(comboAttribut, gc);
+        
+        gc.gridx = 2;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gc.insets =  new Insets(0,0,0,3);
+        add(comboProperty, gc);
+   }
+
+}
