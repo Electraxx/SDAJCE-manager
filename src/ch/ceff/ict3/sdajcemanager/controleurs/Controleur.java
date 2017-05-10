d286880919a45cf247e88b64570cd07d21a0fdbd
cp-14syf <cp-14syf@PC-BD18-03.intra.ceff.ch>
2017-05-10 11:02:45 +0200

    Ajout des méthodes du controleur

--- src/ch/ceff/ict3/sdajcemanager/controleurs/Controleur.java
+++ src/ch/ceff/ict3/sdajcemanager/controleurs/Controleur.java
@@ -5,9 +5,12 @@
  */
 package ch.ceff.ict3.sdajcemanager.controleurs;
 
-import ch.ceff.ict3.sdajcemanager.event.AddCarteEvent;
+import ch.ceff.ict3.sdajcemanager.event.*;
 import ch.ceff.ict3.sdajcemanager.modele.Carte;
+import ch.ceff.ict3.sdajcemanager.modele.Conteneur;
 import ch.ceff.ict3.sdajcemanager.modele.Database;
+import ch.ceff.ict3.sdajcemanager.modele.Deck;
+import ch.ceff.ict3.sdajcemanager.modele.Partie;
 
 /**
  *
@@ -21,9 +24,45 @@ public class Controleur {
         db = db.newDatabase();
     }
 
-
-    public void AddCarte(AddCarteEvent event) {
-        Carte carte = new Carte(-1,event.getNom(),event.getType(), event.getSphere(), event.getNombre(), event.getConteneur());
+    public void addCarte(AddCarteEvent event) {
+        Carte carte = new Carte(-1, event.getNom(), event.getType(), event.getSphere(), event.getNombre(), event.getConteneur());
         db.addCarte(carte);
     }
+
+    public void delCarte(int index) {
+        db.delCarte(index);
+    }
+
+    public void editCarte(EditCarteEvent event) {
+        Carte carte = new Carte(event.getId(), event.getNom(), event.getType(), event.getSphere(), event.getNombre(), event.getConteneur());
+        db.editCarte(carte);
+    }
+
+    public void addDeck(AddDeckEvent event) {
+        Deck deck = new Deck(-1, event.getNom(), event.getCartes());
+        db.addDeck(deck);
+    }
+
+    public void delDeck(int index) {
+        db.delDeck(index);
+    }
+
+    public void AddPartie(AddPartieEvent event) {
+        Partie partie = new Partie(-1, event.getDate(), event.isResultat(), event.getDecks());
+        db.addPartie(partie);
+    }
+
+    public void DelPartie(int index) {
+        db.delPartie(index);
+    }
+
+    public void AddConteneur(AddConteneurEvent event) {
+        Conteneur conteneur = new Conteneur(-1, event.getNom(), event.getAbbr());
+        db.addConteneur(conteneur);
+    }
+
+    public void DelConteneur(int index) {
+        db.delConteneur(index);
+    }
+
 }
